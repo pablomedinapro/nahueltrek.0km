b113fa3cf45ff1274b7a3c35f09fdf98d5ae877b
medinattor <medinattor@hotmail.com>
2025-11-06 17:36:23 -0300

    Initial commit - Nahueltrek app con logo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import logo from './assets/logo.png'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={logo} alt="Nahuel Trek Logo" style={{ width: '200px', marginBottom: '20px' }} />
+      <h1>Nahueltrek</h1>
+      <p>Bienvenido a mi aplicación</p>
+    </div>
+  )
+}
+
+export default App
